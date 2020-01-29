ff10222758c711b273131346d9166dfb023ccdde
Antony Sanders <antony.devon92@gmail.com>
2020-01-29 00:08:14 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT serires.title
+  "SELECT series.title
     FROM series 
     JOIN characters
     ON characters.id = character_books.character_id 
